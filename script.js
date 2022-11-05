5e9ca74ef5865d358c4ece4b4a7d42d478c18367
Naveen C Tom <naveenctom514@gmail.com>
2022-11-05 11:30:47 +0530

    log changed photo added about changed

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
 
-window.addEventListener('DOMContentLoaded', event => {
-
-    // Activate SimpleLightbox plugin for portfolio items
-    new SimpleLightbox({
-        elements: '#portfolio a.portfolio-box'
-    });
-
-});
-
 
 let btn = document.getElementById('btn');
 
